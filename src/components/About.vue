e2d1ced71785ad76d098b9d4429ad4c9922eac9e
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2020-03-10 13:41:58 +0700

    Finished list of skills with VueJS

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+        <h1>Hello {{name}}, This is an about page</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem animi iure voluptate, facilis reiciendis amet nobis omnis obcaecati reprehenderit eligendi dolorem explicabo impedit molestias pariatur et modi a! Unde, facilis.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data() {
+        return {
+            name: this.$route.params.name
+        }
+    }
+}
+</script>
